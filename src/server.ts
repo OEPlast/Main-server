c6811b5598a175f8bf02a4f27a6d9e80ab20f317
chocoscoding <chocoscoding@gmail.com>
2025-11-28 21:38:40 +0100

    changed cors settings

--- src/server.ts
+++ src/server.ts
@@ -68,19 +68,10 @@ const allowedOrigins = process.env.CORS_ORIGINS
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+     origin: allowedOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization','x-request-id',],
   })
 );
 
